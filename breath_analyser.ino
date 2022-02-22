c72df252dd5a4184a2a96fcb251a3ea0a707e7af
Harshit Thakur <96690206+mauser0007@users.noreply.github.com>
2022-02-23 01:08:10 +0530

    Add files via upload

---
+++ breath_analyser.ino
@@ -0,0 +1,209 @@
+int smoke = 0;
+
+void setup()
+{
+  pinMode(A0, INPUT);
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(3,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  smoke = analogRead(A0);
+  Serial.println(smoke);
+  delay(10);
+  
+  // Glow LED's based on intensity of smoke to indicate alcohol level
+  if(smoke >= 90)
+  {
+    digitalWrite(13,HIGH);
+    digitalWrite(12,HIGH);
+    digitalWrite(11,HIGH);
+    digitalWrite(10,HIGH);
+    digitalWrite(9,HIGH);
+    digitalWrite(8,HIGH);
+    digitalWrite(7,HIGH);
+    digitalWrite(6,HIGH);
+    digitalWrite(5,HIGH);
+    digitalWrite(4,HIGH);
+    digitalWrite(3,HIGH);
+  }
+  else
+  {
+    if(smoke >84)
+    {
+      digitalWrite(13,HIGH);
+    digitalWrite(12,HIGH);
+    digitalWrite(11,HIGH);
+    digitalWrite(10,HIGH);
+    digitalWrite(9,HIGH);
+    digitalWrite(8,HIGH);
+    digitalWrite(7,HIGH);
+    digitalWrite(6,HIGH);
+    digitalWrite(5,HIGH);
+    digitalWrite(4,LOW);
+    digitalWrite(3,LOW);
+    }
+    else
+    {
+      if(smoke >79)
+      {
+        digitalWrite(13,HIGH);
+        digitalWrite(12,HIGH);
+        digitalWrite(11,HIGH);
+        digitalWrite(10,HIGH);
+        digitalWrite(9,HIGH);
+        digitalWrite(8,HIGH);
+        digitalWrite(7,HIGH);
+        digitalWrite(6,HIGH);
+        digitalWrite(5,LOW);
+        digitalWrite(4,LOW);
+        digitalWrite(3,LOW);
+      }
+      else
+      {
+        if(smoke >74)
+        {
+          digitalWrite(13,HIGH);
+          digitalWrite(12,HIGH);
+          digitalWrite(11,HIGH);
+          digitalWrite(10,HIGH);
+          digitalWrite(9,HIGH);
+          digitalWrite(8,HIGH);
+          digitalWrite(7,HIGH);
+          digitalWrite(6,LOW);
+          digitalWrite(5,LOW);
+          digitalWrite(4,LOW);
+          digitalWrite(3,LOW);
+        }
+        else
+        {
+          if(smoke >69)
+          {
+            digitalWrite(13,HIGH);
+            digitalWrite(12,HIGH);
+            digitalWrite(11,HIGH);
+            digitalWrite(10,HIGH);
+            digitalWrite(9,HIGH);
+            digitalWrite(8,HIGH);
+            digitalWrite(7,LOW);
+            digitalWrite(6,LOW);
+            digitalWrite(5,LOW);
+            digitalWrite(4,LOW);
+            digitalWrite(3,LOW);
+          }
+           else
+           {
+             if(smoke >64)
+             {
+                digitalWrite(13,HIGH);
+                digitalWrite(12,HIGH);
+                digitalWrite(11,HIGH);
+                digitalWrite(10,HIGH);
+                digitalWrite(9,HIGH);
+                digitalWrite(8,LOW);
+                digitalWrite(7,LOW);
+                digitalWrite(6,LOW);
+                digitalWrite(5,LOW);
+                digitalWrite(4,LOW);
+                digitalWrite(3,LOW);
+             }
+             else
+             {
+               if(smoke >59)
+               {
+                 digitalWrite(13,HIGH);
+                 digitalWrite(12,HIGH);
+                 digitalWrite(11,HIGH);
+                 digitalWrite(10,HIGH);
+                 digitalWrite(9,LOW);
+                 digitalWrite(8,LOW);
+                 digitalWrite(7,LOW);
+                 digitalWrite(6,LOW);
+                 digitalWrite(5,LOW);
+                 digitalWrite(4,LOW);
+                 digitalWrite(3,LOW);
+               }
+               else
+               {
+                 if(smoke >49)
+                 {
+                   digitalWrite(13,HIGH);
+                   digitalWrite(12,HIGH);
+                   digitalWrite(11,HIGH);
+                   digitalWrite(10,LOW);
+                   digitalWrite(9,LOW);
+                   digitalWrite(8,LOW);
+                   digitalWrite(7,LOW);
+                   digitalWrite(6,LOW);
+                   digitalWrite(5,LOW);
+                   digitalWrite(4,LOW);
+                   digitalWrite(3,LOW);
+                 }
+                 else
+                 {
+                   if(smoke >39)
+                   {
+                      digitalWrite(13,HIGH);
+                      digitalWrite(12,HIGH);
+                      digitalWrite(11,LOW);
+                      digitalWrite(10,LOW);
+                      digitalWrite(9,LOW);
+                      digitalWrite(8,LOW);
+                      digitalWrite(7,LOW);
+                      digitalWrite(6,LOW);
+                      digitalWrite(5,LOW);
+                      digitalWrite(4,LOW);
+                      digitalWrite(3,LOW);
+                   }
+                   else
+                   {
+                     if(smoke >24)
+                     {
+                         digitalWrite(13,HIGH);
+                         digitalWrite(12,LOW);
+                         digitalWrite(11,LOW);
+                         digitalWrite(10,LOW);
+                         digitalWrite(9,LOW);
+                         digitalWrite(8,LOW);
+                         digitalWrite(7,LOW);
+                         digitalWrite(6,LOW);
+                         digitalWrite(5,LOW);
+                         digitalWrite(4,LOW);
+                         digitalWrite(3,LOW);
+                     }
+                     else
+                     {
+                          digitalWrite(13,LOW);
+                          digitalWrite(12,LOW);
+                          digitalWrite(11,LOW);
+                          digitalWrite(10,LOW);
+                          digitalWrite(9,LOW);
+                          digitalWrite(8,LOW);
+                          digitalWrite(7,LOW);
+                          digitalWrite(6,LOW);
+                          digitalWrite(5,LOW);
+                          digitalWrite(4,LOW);
+                          digitalWrite(3,LOW);
+                     }
+                   }
+                 }
+               }
+             }
+           }
+          }
+        }
+      }
+    }
+  delay(10);
+}
